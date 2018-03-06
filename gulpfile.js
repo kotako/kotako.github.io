12e308df29597b76a8160f4e70a5abbfee4a16aa
Takorras <sy.empress0321@gmail.com>
2018-03-06 11:28:36 +0900

    リンクだけ置いた

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var fs = require('fs');
+var pkg = JSON.parse(fs.readFileSync('./package.json'));
+var assetsPath = path.resolve(pkg.path.assetsDir);
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+
+gulp.task('sass', function() {
+  console.log("sass compiling");
+  return gulp.src(path.join(assetsPath, 'sass/index.scss'))
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(gulp.dest(path.join(assetsPath, 'css/')));
+});
+
+gulp.task('default', function() {
+    gulp.watch(path.join(assetsPath, 'sass/**/*.scss'),['sass']);
+});
